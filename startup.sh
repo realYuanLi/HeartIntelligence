855b943a7152b916d5cd5a09386f7ed8db9a4884
Yuan Li <yl967@cam.ac.uk>
2025-09-25 11:33:18 +0800

    Add startup script

--- startup.sh
+++ startup.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-exec gunicorn --bind=0.0.0.0${PORT} --timeout 600 app:app
+set -e
+PORT="${PORT:-8000}"
+
+exec gunicorn "app:app" \
+  -k uvicorn.workers.UvicornWorker \
+  --bind "0.0.0.0:${PORT}" \
+  --workers 2 \
+  --timeout 600
